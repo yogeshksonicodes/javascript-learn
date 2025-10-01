154cd7213d8aa44e96f1c87182d5c86a50a92d7b
Yogesh  Kumar soni <153350265+yogeshksonicodes@users.noreply.github.com>
2025-10-01 19:13:50 +0530

    Update javascript03.js

---
+++ javascript03.js
@@ -0,0 +1,25 @@
+let score ="33"
+let score1 =33
+console.table([typeof score, typeof(score)])
+console.table([typeof score1, typeof(score1)])
+let valueInNumber=Number(score);
+console.table([typeof valueInNumber, typeof(valueInNumber)])
+/*
+ 33= 33
+ 33abc= NAN
+ ture=1,  flase=0
+*/
+let isLoggedIn=0
+let boolean= Boolean(isLoggedIn)
+console.log(boolean);
+
+let isLoggedIn1=1
+let boolean1= Boolean(isLoggedIn1)
+console.log(boolean1);
+let isLoggedIn2=" "
+let boolean2= Boolean(isLoggedIn2)
+console.log(boolean2);
+let isLoggedIn3="yogesh"
+let boolean3= Boolean(isLoggedIn3)
+console.log(boolean3);
+ let someNumeber =33
