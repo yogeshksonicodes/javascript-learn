356661357275fea5483e8a3ac14525a7dedc98a5
Yogeshk.soni <Yogeshkumarsoni.in@gmail.com>
2025-10-01 16:57:40 +0530

    first commit

---
+++ javascrit1.js
@@ -0,0 +1,15 @@
+console.log('Yogesh')
+
+/* Let , const, Var
+ variable: it is a container. where you can store data.
+*/
+ 
+const accountId=1443456;
+let  accountEmail='yogesh@gmail.com';
+var password='Yogesh@123';
+accountCity=" jaipur ";
+/*accountId=123435;*/
+accountEmail='yogesh@1234';
+password='yogesh@12343';
+accountCity="Pune"; 
+console.table({accountEmail,accountId,password,accountCity});
